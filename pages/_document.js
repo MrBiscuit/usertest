f972a32ee28e73445f06091fbde193fcec380a1b
mrbiscuit <sunshuaiqi@gmail.com>
2023-03-27 14:11:31 +0200

    23

--- pages/_document.js
+++ pages/_document.js
@@ -15,14 +15,23 @@ class MyDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-                (function(h,o,t,j,a,r){
-                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                  h._hjSettings={hjid:3417245,hjsv:6};
-                  a=o.getElementsByTagName('head')[0];
-                  r=o.createElement('script');r.async=1;
-                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                  a.appendChild(r);
-                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+                (function (m, a, z, e) {
+                  var s, t;
+                  try {
+                    t = m.sessionStorage.getItem('maze-us');
+                  } catch (err) {}
+                  if (!t) {
+                    t = new Date().getTime();
+                    try {
+                      m.sessionStorage.setItem('maze-us', t);
+                    } catch (err) {}
+                  }
+                  s = a.createElement('script');
+                  s.src = z + '?t=' + t + '&apiKey=' + e;
+                  s.async = true;
+                  a.getElementsByTagName('head')[0].appendChild(s);
+                  m.mazeUniversalSnippetApiKey = e;
+                })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', '03da466a-f372-4a67-99a5-61e5418cadc8');
               `,
             }}
           />
